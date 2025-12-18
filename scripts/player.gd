3c46f9d87659f32fb1a4b7ebeda7dac2b8720561
TommyVanAcker <tommyalphonsvanacker@gmail.com>
2025-12-18 10:41:24 +0100

    clamp the player position

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,12 @@ extends CharacterBody2D
 
 @export var speed = 100
 
+
+
+
 func _physics_process(delta: float) -> void:
+	var screensize  = get_viewport_rect().size
+
 	velocity = Vector2(0,0)
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -speed
@@ -13,3 +18,19 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("move_down"):
 		velocity.y = speed
 	move_and_slide()
+	#clamp the player to the screensize so it can not go outside the screen area
+	#global_position.x = clampf(global_position.x, 0, screensize.x)
+	#global_position.y = clampf(global_position.y, 0, screensize.y)
+	
+	#clampf can also be done like the below but is with much more code
+	#if global_position.x < 0:
+	#	global_position.x = 0
+	#elif global_position.x > screensize.x:
+	#	global_position.x = screensize.x
+	#if global_position.y < 0:
+	#	global_position.y = 0
+	#elif global_position.y > screensize.y:
+	#	global_position.y = screensize.y
+		
+	#global position itself also has a clamp position
+	global_position = global_position.clamp(Vector2(0,0),screensize)
