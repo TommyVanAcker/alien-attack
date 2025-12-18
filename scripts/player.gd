8af409ba0f6206e30b8489d7ffa9c0496411de9a
TommyVanAcker <tommyalphonsvanacker@gmail.com>
2025-12-18 10:07:16 +0100

    player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+@export var speed = 100
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2(0,0)
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	elif Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	move_and_slide()
